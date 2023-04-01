8a2f05e716bd21ddfff07940ef5486e28a2472ac
Bridget Mailley <bmailley42@gmail.com>
2023-04-01 18:32:04 +0200

    added api dropdown, updated styling

--- code/src/components/TrendingList.js
+++ code/src/components/TrendingList.js
@@ -7,18 +7,23 @@ import { Oval } from 'react-loader-spinner';
 const TrendingList = () => {
   const [movieList, setMovieList] = useState([])
   const [loading, setLoading] = useState(false)
+  const [listChoice, setListChoice] = useState('trending/movie/day')
+
+  const handleListSelection = (event) => {
+    setListChoice(event.target.value)
+  }
 
   useEffect(() => {
     setLoading(true);
 
-    fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_KEY}`)
+    fetch(`https://api.themoviedb.org/3/${listChoice}?api_key=${process.env.REACT_APP_KEY}`)
       .then((response) => response.json())
       .then((data) => setMovieList(data.results))
       .catch((error) => console.log(error))
       .finally(() => { setLoading(false) })
-  }, [])
+  }, [listChoice])
   return (
-    <section className="front-page">
+    <>
       {loading && (
         <Oval
           ariaLabel="loading-indicator"
@@ -29,18 +34,31 @@ const TrendingList = () => {
           color="black"
           secondaryColor="white" />
       )}
-      {movieList.map((movie) => {
-        return (
-          <Link key={movie.id} to={`/movies/${movie.id}`}>
-            <img src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="poster" />
-            <div className="poster-hover-details">
-              <h1>{movie.title}</h1>
-              <p>Released {movie.release_date}</p>
-            </div>
-          </Link>
-        )
-      })}
-    </section>
+      <div className="list-selector">
+        <h1>&#127903;&#65039;&#127871; The Movie Page &#127871;&#127903;&#65039;</h1>
+        <p>Would you like to see movies that are: </p>
+        <select value={listChoice} onChange={handleListSelection}>
+          <option value disabled>Please select one:</option>
+          <option value="trending/movie/day">Trending</option>
+          <option value="movie/top_rated">Top Rated</option>
+          <option value="movie/upcoming">Upcoming</option>
+          <option value="movie/now_playing">Now Playing</option>
+        </select>
+      </div>
+      <section className="front-page">
+        {movieList.map((movie) => {
+          return (
+            <Link key={movie.id} to={`/movies/${movie.id}`}>
+              <img src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="poster" />
+              <div className="poster-hover-details">
+                <h1>{movie.title}</h1>
+                <p>Released {movie.release_date}</p>
+              </div>
+            </Link>
+          )
+        })}
+      </section>
+    </>
   )
 }
 
